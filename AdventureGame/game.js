a6d94969877aa5865616104bd48fdaf33aaaad2d
Unknown <noah.f250@stu.nebo.edu>
2019-01-23 10:26:34 -0700

    started html stuff

---
+++ AdventureGame/game.js
@@ -0,0 +1,18 @@
+// Single line comment
+/*
+
+Multiline comment
+
+*/
+
+//document.write("<h1>So steez dude, so <em>STEEZ</em>.</h1>");
+
+//alert("WARNING! WARNING! WARNING! REEEEEE!");
+
+//Confirm("do you like cheese?);
+
+//prompt("what kind of cheese do you like?);
+
+var PC = prompt("What is your name?")
+
+confirm("So your name is "+PC)
