9381c49ef7644c06d3e4ad6791fb07eb370376d7
Unknown <noah.f250@stu.nebo.edu>
2019-02-06 10:26:44 -0700

    worked on game

--- AdventureGame/game.js
+++ AdventureGame/game.js
@@ -1,22 +1,3 @@
-// Single line comment
-/*
-
-Multiline comment
-
-*/
-
-//document.write("<h1>So steez dude, so <em>STEEZ</em>.</h1>");
-
-//alert("WARNING! WARNING! WARNING! REEEEEE!");
-
-//Confirm("do you like cheese?);
-
-//prompt("what kind of cheese do you like?);
-
-//var PC = prompt("What is your name?");
-
-//confirm("So your name is "+PC);
-
         var inventory = {
             credits:1000,
             lazerSword:0,
@@ -37,11 +18,6 @@ function Game(){
     function Cell(){
         var jail = prompt("You slowly start to wake up. Your head is throbbing and in great pain. You try to remember what happened last night. You look around and notice your in some kind of jail cell...but the gravity feels low...and unearthly... \n -Look around \n -call for someone \n -look out the window \n -go to shop demonstration");
         
-        if(jail == "shop" || "shop demonstration"){
-        
-        shop();
-        }
-        
         if(jail == "look around" || jail == "look"){
         
         LOOK();
@@ -51,6 +27,11 @@ function Game(){
          
         Window();   
      }
+        
+        else if(jail == "shop" || "shop demonstration"){
+        
+        shop();
+        }
     
 }
         function Window(){
@@ -61,25 +42,15 @@ function Game(){
             var cellLook = prompt("You take a good look around. Behind you is your bed with a window, on your right you have a sink and toilet, on your left is an old wall, and directly in front of you is a row of lazer beams that look like they would cut or kill anything that goes through. would you like to \n -inspect window \n -inspect old wall \n -inspect toilet and sink \n -try to go through lazers.");
             
             if(cellLook == "try to go through lazers" || "lazers" || "go through lazers"){
-                alert("You back up as far as you can to the wall. Charging at the lazers at full speed you make it though...in three different pieces...good job, you successfully killed yourself and now you have to restart the game.");
-            }
-            
-            else if(cellLook == "inspect old wall" || "old wall"){
-                var wallInt = prompt("You inspect the old wall, you see that it is cracked and possibly breakable. \n -kick wall n\ -throw your body into the wall n\ -leave wall alone");
                 
-                if(wallInt == "kick" || "kick wall"){
-                    var kickingWall = prompt("You kick the wall with all your might. the crack gets bigger, but dust falls from the ceiling. it might be dangerous to kick again, but it could lead to a way out. n\ -kick the wall again n\ back away");
-                    
-                    if(kickingWall == "kick again" || "kick"){
-                        var wallFallsDown = prompt("You kick the wall as hard as you can once more!...but the ceiling collapses and your leg gets hit with debris. You can either n\ -make a run for it! n\ -wait for the guards and lie.");
-                        
-                        if(wallFallsDown == "make a run for it" || "run"){
-                            alert("You make a break for it!...but you have no idea where your running. You limp run out of your cell and head to the door. The door slides open and you hear the guards running from the right. you break left and make it 30 feet before you get shot in the chest and fall to the floor. Your attempt at escape was brave but you could not outrun a lazer beam.")
-                        }
-                    }
-                }
+                Lazarbeam();
+            }
+            else if(cellLook == "inspect old wall" || "old wall"){
+                
+                Trump();
             }
         }
+        
         
         function Yell(){
             var callForSomeone = prompt("You yell for someone to talk to. Two alien guards open the door and approach you. What do you choose to say? \n -i come in peace \n -why am i in here \n -let me out of here");
@@ -98,21 +69,32 @@ function Game(){
                         //takes money
                         inventory.credits = inventory.credits - 100;
                         //displays coins left
-                        alert("you have "+inventory.credits" credits remaining");
+                        //alert("you have " +inventory.credits " credits remaining");
                         shop();
                     }
-                    if(spaceShop == "plasma bomb" || "buy plasma bomb" && inventory.credits >= 100){
-                        var bombBuy = confrim("do you really want to buy a plasma bomb? you realize if you accidentally set this off you could potentialy destroy hundreds and maybe thousands of planets. oh and you would also be dead. do you really want this?");
-                        if(bombBuy){
-                            inventory.plasmaBomb ++;
-                            alert("you now own " +inventory.plasmaBomb+" plasma bombs");
-                            inventory.credits = inventory.credits - 100;
-                            alert("you have "+inventory.credits" credits remaining");
-                            shop();
-                        }
-                    }
+    
                 }
         }
+    
+        function Trump(){
+            var wallInt = prompt("You inspect the old wall, you see that it is cracked and possibly breakable. \n -kick wall n\ -throw your body into the wall n\ -leave wall alone");
+                
+                    if(wallInt == "kick" || "kick wall"){
+                        var kickingWall = prompt("You kick the wall with all your might. the crack gets bigger, but dust falls from the ceiling. it might be dangerous to kick again, but it could lead to a way out. n\ -kick the wall again n\ back away");
+                    
+                        if(kickingWall == "kick again" || "kick"){
+                            var wallFallsDown = prompt("You kick the wall as hard as you can once more!...but the ceiling collapses and your leg gets hit with debris. You can either n\ -make a run for it! n\ -wait for the guards and lie.");
+                        
+                        if(wallFallsDown == "make a run for it" || "run"){
+                            alert("You make a break for it!...but you have no idea where your running. You limp run out of your cell and head to the door. The door slides open and you hear the guards running from the right. you break left and make it 30 feet before you get shot in the chest and fall to the floor. Your attempt at escape was brave but you could not outrun a lazer beam.")
+                            }
+                        }
+                    }
+            
+            }
         
+        function Lazarbeam(){
+            alert("You back up as far as you can to the wall. Charging at the lazers at full speed you make it though...in three different pieces...good job, you successfully killed yourself and now you have to restart the game.");
+        }
 
 }
