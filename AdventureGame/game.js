75539da6c2edc2e9db12c02f1370e7fe50a69864
Unknown <noah.f250@stu.nebo.edu>
2019-02-08 10:28:34 -0700

    rng system

--- AdventureGame/game.js
+++ AdventureGame/game.js
@@ -12,25 +12,31 @@ function Game(){
     document.write("Trouble in space.");
     var playerName = prompt("what is your name?");
     alert("Welcome to the Interstellar space station "+ playerName);
+    alert("Before you start the game, make sure you spell everything EXACTLY as it reads.");
     
     Cell();
     
     function Cell(){
-        var jail = prompt("You slowly start to wake up. Your head is throbbing and in great pain. You try to remember what happened last night. You look around and notice your in some kind of jail cell...but the gravity feels low...and unearthly... \n -Look around \n -call for someone \n -look out the window \n -go to shop demonstration");
+        var jail = prompt("You slowly start to wake up. Your head is throbbing and in great pain. You try to remember what happened last night. You look around and notice your in some kind of jail cell...but the gravity feels low...and unearthly... \n -Look around \n -call for someone \n -look out the window \n -shop");
         
-        if(jail == "look around" || jail == "look"){
+        if(jail == "look around"){
         
         LOOK();
     }
         
-        else if(jail == "inspect window" || "window"){
+        else if(jail == "inspect window"){
          
         Window();   
      }
         
-        else if(jail == "shop" || "shop demonstration"){
+        else if(jail == "shop"){
         
         shop();
+        }
+        
+        else if(jail == "lazer"){
+            
+        Lazarbeam()
         }
     
 }
@@ -41,11 +47,11 @@ function Game(){
         function LOOK(){
             var cellLook = prompt("You take a good look around. Behind you is your bed with a window, on your right you have a sink and toilet, on your left is an old wall, and directly in front of you is a row of lazer beams that look like they would cut or kill anything that goes through. would you like to \n -inspect window \n -inspect old wall \n -inspect toilet and sink \n -try to go through lazers.");
             
-            if(cellLook == "try to go through lazers" || "lazers" || "go through lazers"){
+            if(cellLook == "try to go through lazers"){
                 
                 Lazarbeam();
             }
-            else if(cellLook == "inspect old wall" || "old wall"){
+            else if(cellLook == "inspect old wall"){
                 
                 Trump();
             }
@@ -59,7 +65,7 @@ function Game(){
         function shop(){
             var spaceShop = prompt("Welcome to the space shop. what would you like to buy? \n -lazer sword \n -plasma bomb \n -ship map \n -beam gun \n -leave");
             
-                if(spaceShop == "lazer sword" || "buy lazer sword" && inventory.credits >= 100){
+                if(spaceShop == "lazer sword" && inventory.credits >= 100){
                     var lazerSwordBuy = confirm("do you really need a lazer sword? you might burn or even kill yourself with it.");
                     if(lazerSwordBuy){
                         //adds sword
@@ -77,24 +83,38 @@ function Game(){
         }
     
         function Trump(){
-            var wallInt = prompt("You inspect the old wall, you see that it is cracked and possibly breakable. \n -kick wall n\ -throw your body into the wall n\ -leave wall alone");
+            var wallInt = prompt("You inspect the old wall, you see that it is cracked and possibly breakable. \n -kick wall \n -throw your body into the wall \n -leave wall alone");
                 
-                    if(wallInt == "kick" || "kick wall"){
+                    if(wallInt == "kick wall"){
                         var kickingWall = prompt("You kick the wall with all your might. the crack gets bigger, but dust falls from the ceiling. it might be dangerous to kick again, but it could lead to a way out. n\ -kick the wall again n\ back away");
                     
-                        if(kickingWall == "kick again" || "kick"){
+                        if(kickingWall == "kick wall again"){
                             var wallFallsDown = prompt("You kick the wall as hard as you can once more!...but the ceiling collapses and your leg gets hit with debris. You can either n\ -make a run for it! n\ -wait for the guards and lie.");
                         
-                        if(wallFallsDown == "make a run for it" || "run"){
+                        if(wallFallsDown == "make a run for it"){
                             alert("You make a break for it!...but you have no idea where your running. You limp run out of your cell and head to the door. The door slides open and you hear the guards running from the right. you break left and make it 30 feet before you get shot in the chest and fall to the floor. Your attempt at escape was brave but you could not outrun a lazer beam.")
                             }
                         }
                     }
             
             }
-        
         function Lazarbeam(){
-            alert("You back up as far as you can to the wall. Charging at the lazers at full speed you make it though...in three different pieces...good job, you successfully killed yourself and now you have to restart the game.");
+            alert("You back up as far as you can. You charge at the lazers with everything you have!");
+            var lazerChance = ["got killed", "made it through alive", "made it through, but you are severely injured"];
+            alert("You "+lazerChance[Math.floor(Math.random()* 2)]+"!");
+                
+            if(lazerChance == 1){
+                darkSide();
+            }
         }
+        function darkSide(){
+            prompt("You're on the other side of your cell. You notice an emergency exit hatch in the corner of the room and a doorway that leads to the rest of the ship. \n -emergency exit \n -doorway")
+        }
+    
+    var rand = Math.Floor(Math.Random()*3);
+    
+    var chance = [death(), win(), lose()];
+    
+    
 
 }
